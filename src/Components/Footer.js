a4a213875e05acfc38e1759a5a6b5a580158a8d1
Arjun A M <amarjun79@gmail.com>
2024-04-21 21:23:38 +0530

    Commit 5

---
+++ src/Components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return <div className='footer'>&copy; Copyrights @ AMANA | 2024</div>;
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ root.render(
     <ListItem />
     <ListItem />
     <ListItem />
+    <Footer />
   </div>
 );
 
